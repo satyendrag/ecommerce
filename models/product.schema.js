0155aef07e7505b52908137bfc8a36fed7a16766
Satyendra <msatyendra531@gmail.com>
2022-12-04 10:36:51 +0530

    Added Product Schema

--- models/product.schema.js
+++ models/product.schema.js
@@ -4,12 +4,43 @@ const productSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Collection name is required"],
-      maxLenght: [120, "Collection name should be less than 120 char"],
+      required: [true, "Product name is required"],
+      maxLenght: [120, "Product name should be less than 120 char"],
       trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Product description is required"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please provide price."],
+    },
+    photos: [
+      {
+        secure_url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    collectionId: {
+      ref: "Collection",
+      type: mongoose.Types.ObjectId,
     },
   },
   {
     timestamps: true,
   }
 );
+
+export default mongoose.Model("productSchema", productSchema);
